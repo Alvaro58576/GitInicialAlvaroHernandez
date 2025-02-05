2a13f4244f55c667f7b4592bc286324d45f48daf
alvaro58576 <alvaro58576@gmail.com>
2025-02-05 09:48:51 +0100

    Añadido método para mostrar fecha y hora

--- Principal.java
+++ Principal.java
@@ -1,4 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        mostrarFechaHora();
+    }
+
+    public static void mostrarFechaHora() {
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        System.out.println("Fecha y hora actual: " + ahora.format(formato));
+    }
+}public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
     }
